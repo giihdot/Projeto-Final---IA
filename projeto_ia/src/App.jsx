0c507b445f8cbd5c8057bf32b6698fb13bdd3dec
Duarte <dionysiomariaduarte2808@gmail.com>
2025-11-27 14:24:46 -0300

    App

--- projeto_ia/src/App.jsx
+++ projeto_ia/src/App.jsx
@@ -1,27 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import NavBar from "./components/NavBar";
+
 import Apresentacao from "./pages/Apresentacao";
 import Professora from "./pages/SobreProfessora";
 import Calendario from "./pages/Calendario";
 import Depoimentos from "./pages/Depoimentos";
 import Colabore from "./pages/SobreNos";
 
+// IMPORTA O CHAT
+import IA from "./pages/IA";
+
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-    <NavBar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Apresentacao />} />
         <Route path="/professora" element={<Professora />} />
         <Route path="/calendario" element={<Calendario />} />
         <Route path="/depoimentos" element={<Depoimentos />} />
         <Route path="/asgatitas" element={<Colabore />} />
+
+        {/* NOVA ROTA DO CHAT */}
+        <Route path="/ia" element={<IA />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-
-
-
